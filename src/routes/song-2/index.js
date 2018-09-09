815f2fa992c39f1949c45ba18850565e634bb1f1
Hakan Alpay <hak7alp@gmail.com>
2018-09-09 12:48:14 -0700

    fixed soundcloud on mobile miraculously

--- src/routes/song-2/index.js
+++ src/routes/song-2/index.js
@@ -8,7 +8,11 @@ const Song2 = () => (
 		<h2>wrap it</h2>
 		<p>Rollomatik</p>
 		<a href="https://soundcloud.com/rollomatik/rollomatik-wrap-it" target="_blank">original track</a>
-		<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/35574596&color=%23ff00c8&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+		<div class={style.sound}>
+			<iframe id={style.frame} width="20%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/35574596&color=%23ff00c8&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+		</div>
+		
 	</div>
 );
 
--- src/routes/song/index.js
+++ src/routes/song/index.js
@@ -9,7 +9,11 @@ const Song = () => (
 		<p>CMC-Silenta</p>
 		<p>feat. Mystro & Malenda</p>
 		<a href="https://soundcloud.com/cmc-silenta/01_yeah-thats-right_feat-mystro-malenda_m1" target="_blank">original track</a>
-		<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287161495&color=%23ff00c8&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+		<div class={style.sound}>
+			<iframe id={style.frame} width="20%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287161495&color=%23ff00c8&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+		</div>
+		
 	</div>
 );
 
